192b564f0eabe908fffa4cdb3bb26ae011eec112
Samus9001 <sameer99@outlook.com>
2022-12-21 19:13:27 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>() { "rabbit", "blanket", "glitter", "cranium" };
+
+            var random = new Random();
+            int index = random.Next(words.Count);
+
+            //Console.WriteLine($"LIST 1: {words[0]}");
+
+            //outputs a random element from the list index
+            Console.WriteLine($"{words[index]}");
+
+            string guess = Console.ReadLine();
+        }
+    }
+}
