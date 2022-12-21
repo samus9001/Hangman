f0beab9fa0d00c7cb1d34f50f77ac581cd572267
Samus9001 <sameer99@outlook.com>
2022-12-21 19:49:32 +0000

    #1 Created a function that checks if the list element is part of the guess string

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             List<string> words = new List<string>() { "rabbit", "blanket", "glitter", "cranium" };
-
+            
             var random = new Random();
             int index = random.Next(words.Count);
 
