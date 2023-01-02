b69710316afdcbb6b6571e9475f9437b3cca3851
Samus9001 <sameer99@outlook.com>
2023-01-02 20:27:54 +0000

    #1 Changed string check function to use a for loop in order to loop through the word and compare the guess letter to each letter in the word, then tell the user if there is a match or not

--- Program.cs
+++ Program.cs
@@ -8,18 +8,26 @@
 
             var random = new Random();
             int index = random.Next(words.Count);
+            char guess;
 
-            //outputs a random element from the list index
+            //outputs a random element from the list index (for testing)
             Console.WriteLine($"{words[index]}");
 
-            //allows the input to only be one character
-            char guess = Console.ReadKey().KeyChar;
-            string userGuess = Char.ToString(guess);
-
-            //checks if the guess contains the matching element within it
-            if (words[index].Contains(userGuess, StringComparison.OrdinalIgnoreCase))
+            for (int i = 0; i < words[index].Length; i++)
             {
-                Console.WriteLine("\nThat's a match!");
+                //allows the input to only be one character
+                guess = Console.ReadKey().KeyChar;
+
+                //checks if the guess contains the matching element within it
+                if (words[index][i] == guess)
+                {
+                    Console.WriteLine("\nThat is a match!");
+
+                }
+                else
+                {
+                    Console.WriteLine("\nThat is not a match");
+                }
             }
         }
     }
