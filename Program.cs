40d707f96896a92102cf91d195a74a4788e05f2c
Samus9001 <sameer99@outlook.com>
2023-01-02 23:40:31 +0000

    Tidied comments and code layout

--- Program.cs
+++ Program.cs
@@ -9,34 +9,36 @@
             //selects random element from the list
             var random = new Random();
             int index = random.Next(words.Count);
-            string word = words[index];
-            string hiddenWord = "";
 
             //variables that track the game state
             char guess;
             int incorrectGuess = 0;
-            bool correctGuess = false;
-            bool gameWon = false;
+            bool correctGuess = false; //not implemented yet
+            bool gameWon = false; //not implemented yet
+
+            string word = words[index];
+            string hiddenWord = "";
 
 
             Console.WriteLine("Welcome to Hangman. You must guess the word in 8 tries.\nEnter a character for your guess: \n");
 
-            //outputs the chosen word (for testing)
-            Console.WriteLine(word);
-            Console.WriteLine(word.Length);
-
+            //loop that sets the hiddenWord string length to the same as the chosen element
             for (int i = 0; i < word.Length; i++)
             {
                 hiddenWord += "_";
+
             }
 
-            Console.WriteLine(hiddenWord);  // Output: "xxxxx"
+            //displays the length and correclty guessed letters of the chosen word to the user
+            Console.WriteLine(hiddenWord);
+            Console.WriteLine(); //new line
 
             //main game loop
             while (true)
             {
                 //makes the input one character only
                 guess = Console.ReadKey().KeyChar;
+                Console.WriteLine(); //new line
 
                 //loops through each character in the element
                 for (int i = 0; i < word.Length; i++)
@@ -44,31 +46,33 @@
                     //comparison check to see if the element character matches the guess character
                     if (word[i] == guess)
                     {
-                        Console.WriteLine("\nThat is a match!\n");
+                        //updates the hiddenWord string to remove the character at the same position of the correctly guessed letter in the element and inserts the correct letter
                         hiddenWord = hiddenWord.Remove(i, 1).Insert(i, guess.ToString());
+                        Console.Clear();
                     }
                 }
 
                 Console.WriteLine(hiddenWord);
 
+
                 //checks if the guess character is not present in the element
                 if (!word.Contains(guess, StringComparison.OrdinalIgnoreCase))
                 {
                     incorrectGuess++;
-                    //outputs the number of incorrect guesses (for testing)
-                    Console.WriteLine("\nincorrect guess = " + incorrectGuess);
-                    Console.WriteLine("\nThat is not a match\n");
-                }
-
-                if (gameWon)
-                {
-                    Console.WriteLine("\nYou won!\n");
-                    break;
+                    Console.WriteLine(); //new line
+                    Console.WriteLine("\nThat is incorrect\n");
+                    Console.WriteLine(); //new line
                 }
 
                 if (incorrectGuess >= 8)
                 {
                     Console.WriteLine("\nGame over");
+                    break;
+                }
+
+                if (gameWon) //not implemented yet
+                {
+                    Console.WriteLine("\nYou won!\n");
                     break;
                 }
             }
